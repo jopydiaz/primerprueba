21d95c48f6024ea88cf8d5ff7b8d4d9117c7ea26
Jose Maria Diaz <Jose Maria Diaz>
2016-01-22 14:15:05 -0300

    comentario

---
+++ public/core.js
@@ -0,0 +1,50 @@
+angular.module('MainApp', [])
+
+function mainController($scope, $http) {
+	$scope.newPersona = {};
+	$scope.personas = {};
+	$scope.selected = false;
+
+	// Obtenemos todos los datos de la base de datos
+	$http.get('/api/persona').success(function(data) {
+		$scope.personas = data;
+	})
+	.error(function(data) {
+		console.log('Error: ' + data);
+	});
+
+	// registro a una persona
+	$scope.registrarPersona = function() {
+		$http.post('/api/persona', $scope.newPersona)
+		.success(function(data) {
+				$scope.newPersona = {}; // Borramos los datos del formulario
+				$scope.personas = data;
+			})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+	};
+    
+    // Función que borra un objeto persona conocido su id
+	$scope.borrarPersona = function(newPersona) {
+		$http.delete('/api/persona/' + $scope.newPersona._id)
+		.success(function(data) {
+			$scope.newPersona = {};
+			$scope.personas = data;
+			$scope.selected = false;
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+	};
+
+	// Función para coger el objeto seleccionado en la tabla
+	$scope.selectPerson = function(persona) {
+		$scope.newPersona = persona;
+		$scope.selected = true;
+		console.log($scope.newPersona, $scope.selected);
+	};
+
+
+
+}
